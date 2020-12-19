7ebed4b1fbba488be559e4926b08145cb86262ba
larisa1992 <pelar1992@mail.ru>
2020-12-19 12:31:55 +0300

    update equirements

--- SurveyDesigner/settings.py
+++ SurveyDesigner/settings.py
@@ -119,8 +119,8 @@ else:
 # }
 
 # для Heroku
-import dj_database_url  
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+# import dj_database_url  
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 # DATABASES = {
